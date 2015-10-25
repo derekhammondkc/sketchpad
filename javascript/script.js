4bdd0674cc2d34e223ff85ec33c8a21908c125a1
John Smalley <john.m.smalley@gmail.com>
2015-10-25 12:22:41 -0500

    Responseive to number of squares

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,12 @@
-var gridNum = 16;
+var gridNum = 32;
+var totalGrid = gridNum * gridNum;
+var totalWidth = 600;
+var square = (totalWidth / gridNum) - 2;
 
 $(document).ready(function () {
-	for (var i = 1; i <= 16; i++) {
-		console.log("Hello number: " + i);
+	for (var i = 1; i <= totalGrid; i++) {
+		$('#container').append('<div id="' + i + '">' + i + '</div>')
 	};
+	$('#container > div').css({"width": square,
+								"height": square});
 })
