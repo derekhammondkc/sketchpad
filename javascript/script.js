81e1c52b9d65fdd98d766769c21a61f75d4fe7dc
John Smalley <john.m.smalley@gmail.com>
2015-10-25 20:00:53 -0500

    Standard, Color, and Shade are working, still need to commet

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,100 @@
 var gridNum = 8;
-var totalGrid = gridNum * gridNum;
 var totalLength = 580;
 var square = (totalLength / gridNum) - 2;
+var color = 0;
+var opacity = 0;
 
 $(document).ready(function () {
-	
-	for (var i = 1; i <= gridNum; i++) {
-		for (var j = 1; j <= gridNum; j++) {
-			$('#container').append('<div  class="grid" id="' + j + '"></div>')
-		};
+	createGrid(gridNum);
+
+	$('.grid').on('mouseenter', function() {
+		$(this).addClass("highlighted");
+	});
+
+	$('#standard').on('click', function() {
+		$('.grid').remove();
+		$('button').css("background", "#e0e0e0");
+		$('#standard').css("background", "#b0b0b0");
+
+		do {
+			gridNum = prompt("How many grids wide?");
+			if (gridNum < 2 || gridNum > 64) {
+				alert("Grids must be between 2 to 64");
+			}
+		} while(gridNum < 2 || gridNum > 64);
+
+		createGrid(gridNum);
+
+		$('.grid').on('mouseenter', function() {
+			$(this).addClass("highlighted");
+		});
+
+	});
+
+	$('#random').on('click', function() {
+		$('.grid').remove();
+		$('button').css("background", "#e0e0e0");
+		$('#random').css("background", "#b0b0b0");
+		do {
+			gridNum = prompt("How many grids wide?");
+			if (gridNum < 2 || gridNum > 64) {
+				alert("Grids must be between 2 to 64");
+			}
+		} while(gridNum < 2 || gridNum > 64);
+
+		createGrid(gridNum);
+
+		$('.grid').on('mouseenter', function() {
+			color = Math.random();
+			if (color < (1/7)) {
+				$(this).css("background", "#d10000");
+			}else if (color < (2/7)) {
+				$(this).css("background", "#ff6622");
+			}else if (color < (3/7)) {
+				$(this).css("background", "#ffda21");
+			}else if (color < (4/7)) {
+				$(this).css("background", "#33dd00");
+			}else if (color < (5/7)) {
+				$(this).css("background", "#1133cc");
+			}else if (color < (6/7)) {
+				$(this).css("background", "#220066");
+			}else {
+				$(this).css("background", "#330044");
+			}
+		});
+	});
+
+	$('#shade').on('click', function() {
+		$('.grid').remove();
+		$('button').css("background", "#e0e0e0");
+		$('#shade').css("background", "#b0b0b0");
+		do {
+			gridNum = prompt("How many grids wide?");
+			if (gridNum < 2 || gridNum > 64) {
+				alert("Grids must be between 2 to 64");
+			}
+		} while(gridNum < 2 || gridNum > 64);
+
+		createGrid(gridNum);
+
+		$('.grid').on('mouseenter', function() {
+			$(this).addClass("black");
+			$(this).css("opacity", $(this).css("opacity") - 0.1);
+		});
+	});
+
+});
+
+function createGrid(num) {
+	for (var i = 1; i <= num; i++) {
+		for (var j = 1; j <= num; j++) {
+			$('#container').append('<div  class="grid"></div>');
+		}
 		$('#container').append('<div class="newRow"></div>');
-	};
-	
+	}
+
+	square = (totalLength / num) -2;
 	$('#container > .grid').css({"width": square,
 								"height": square});
 
-	$('#container > div').on('mouseenter', function() {
-		$(this).css("background", "yellow");
-	});
-})
+}
