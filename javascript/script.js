2d1687057721bb821836a93acc14c9e26b5d6c02
John Smalley <john.m.smalley@gmail.com>
2015-10-26 18:37:03 -0500

    All there modes are working with comments throughout

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,9 @@ var square = (totalLength / gridNum) - 2; // size of each grid, -2 to account fo
 var color; // Variable to store the random color that gets picked
 
 $(document).ready(function () {
+	// shades the standard button when page loads
+	$('#standard').css("background", "#b0b0b0");
+
 	// creates the first grid on default values
 	createGrid(gridNum);
 
@@ -59,6 +62,8 @@ $(document).ready(function () {
 		// creates the standard grid base on number of grids users wants
 		createGrid(gridNum);
 
+		// highlight each grid to a random color that will be selected
+		// based on a random number
 		$('.grid').on('mouseenter', function() {
 			color = Math.random();
 			if (color < (1/7)) {
@@ -79,11 +84,16 @@ $(document).ready(function () {
 		});
 	});
 
+	// when user clicks shade button
 	$('#shade').on('click', function() {
+		// remove all old grids and shades only the clicked button
 		$('.grid').remove();
 		$('.newRow').remove();
 		$('button').css("background", "#e0e0e0");
 		$('#shade').css("background", "#b0b0b0");
+		
+		// ask user how many grids wides, and validates between
+		// 2 and 64 grids
 		do {
 			gridNum = prompt("How many grids wide?");
 			if (gridNum < 2 || gridNum > 64) {
@@ -91,26 +101,32 @@ $(document).ready(function () {
 			}
 		} while(gridNum < 2 || gridNum > 64);
 
+		// creates the standard grid base on number of grids users wants
 		createGrid(gridNum);
 
+		// highlight each grid black by an additional 0.1 opacity each mouse pass
 		$('.grid').on('mouseenter', function() {
 			$(this).addClass("black");
-			$(this).css("opacity", $(this).css("opacity") - 0.1);
+			$(this).css("opacity", "+=0.1");
 		});
 	});
 
 });
 
+// function used to create a grid pattern based on the user's input
 function createGrid(num) {
 	for (var i = 1; i <= num; i++) {
+		// creates each individual grid square
 		for (var j = 1; j <= num; j++) {
 			$('#container').append('<div  class="grid"></div>');
 		}
+		// creates each new row
 		$('#container').append('<div class="newRow"></div>');
 	}
 
+	// sets the grid width and height based on the number of grids fit in
+	// the grid's container
 	square = (totalLength / num) -2;
 	$('#container > .grid').css({"width": square,
 								"height": square});
-
 }
